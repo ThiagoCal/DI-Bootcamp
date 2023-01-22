aa9daae050794ce3b47c599207ca9b049dd0e4c0
Thiago Calazans <thiagotorrescalazans@gmail.com>
2023-01-22 06:32:37 -0300

    passing all files to one repo

---
+++ Week 2/Day 1/script.js
@@ -0,0 +1,14 @@
+const addressNumber = 7;
+const addressStreet = "Tsidon";
+const country = "Israel";
+
+const globalAddress = `I live in ${addressStreet} ${addressNumber}, in ${country}`
+
+console.log(globalAddress);
+
+
+const birthYear = 1988;
+let futureYear = 2035;
+const myAge = futureYear - birthYear
+const futureAge = `I will be ${myAge} in ${futureYear}`
+console.log(futureAge)
